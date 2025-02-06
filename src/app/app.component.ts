eed9df7bcdc07dd6219894140f87ba6b82d89a1f
Alfredcss <alfredofe.ti22@utsjr.edu.mx>
2025-02-05 21:13:12 -0600

    corecion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ejemplo2';
+
+  //propiedades
+  resultado:string ='';
+  valorDado1:number = this.generarAleatorio();
+  valorDado2:number = this.generarAleatorio();
+  valorDado3:number = this.generarAleatorio();
+
+  //función que genera un numero aleatorio entre 1 y 6
+  generarAleatorio(){
+    return Math.floor(Math.random()*6)+1;
+  }
+
+  //funcion que hace la tirada y verifica el resultado
+  tirarDados(){
+    this.valorDado1 = this.generarAleatorio();
+    this.valorDado2 = this.generarAleatorio();
+    this.valorDado3 = this.generarAleatorio();
+
+    //comparamos si los 3 son iguales
+    if(this.valorDado1==this.valorDado2 && this.valorDado1==this.valorDado3){
+      this.resultado='Felicidades, Ganaste!!!';
+    } else{
+      this.resultado='Lastima, Vuelve a jugar';
+    }
+  }
 }
